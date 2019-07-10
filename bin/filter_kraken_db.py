73ecdcd86c1c3e29427faa54e3a0d91f623f416a
Asaf Peer <Asaf.Peer@jax.org>
2019-07-10 13:22:56 -0400

    pass parent not found

--- bin/filter_kraken_db.py
+++ bin/filter_kraken_db.py
@@ -33,7 +33,7 @@ def process_command_line(argv):
     if argv is None:
         argv = sys.argv[1:]
     parser = argparse.ArgumentParser(
-        description='Filter a kraken DB file according to its domain predictions',
+        description='Filter a kraken DB file according to its domain predictions. Only filter Bacteria that looks like Eukaryota and Eukaryota that looks like Bacteria.',
         formatter_class=argparse.HelpFormatter)
     parser.add_argument(
         '--dbfile', default = ":memory:",
@@ -47,21 +47,6 @@ def process_command_line(argv):
     parser.add_argument(
         '--taxonomy',
         help="path taxonomy directory of kraken2 DB, should contain names.dmp, nodes.dmp and *accession2taxid files")
-    parser.add_argument(
-        '--filter_virus', default=False, action='store_true',
-        help='By default keep all sequences originating from viral genomes, use this to filter them')
-    parser.add_argument(
-        '--filter_archaea', default=False, action='store_true',
-        help='By default keep all sequences originating from archaea genomes, use this to filter them')
-    parser.add_argument(
-        '--filter_enviro', default=False, action='store_true',
-        help='Set to remove environmental samples')
-    parser.add_argument(
-        '--trust_archaea', default=False, action='store_true',
-        help='By default keep all sequences that map to archaea, use this to filter them')
-    parser.add_argument(
-        '--trust_viruses', default=False, action='store_true',
-        help='By default keep all sequences that map to viruses, use this to filter them')
     parser.add_argument(
         'fasta',
         help='Input fasta file. Headers must be accession numbers or in the format: >kraken:taxid|214684|NC_006670.1')
@@ -127,7 +112,7 @@ def build_db(dbfile, taxdir):
     conn.close()
 
 
-def match_txid(domain, taxid, curr, keepvir=False, keepenv=False, keeparch=False):
+def match_txid(domain, taxid, curr, kingdoms):
     """
     Return True if the txid is under the domain
     """
@@ -136,17 +121,12 @@ def match_txid(domain, taxid, curr, keepvir=False, keepenv=False, keeparch=False
             (p,) = curr.execute("select parent from nodes where taxid=?", (taxid, )).next()
         except:
             logging.warn("Can't find parent for taxonomy {}".format(taxid))
-            break
+            return True
         if p == taxid:
             break
-        if (taxid == 10239 and not keepvir):
-            return True
-        if (taxid == 48479 and not keepenv):
-            return False
-        if (taxid == 2157 and not keeparch):
-            return True
         taxid = p
-
+        if (taxid in kingdoms) and (taxid!=domain):
+            return False 
     return taxid == domain 
 
 
@@ -168,11 +148,10 @@ def main(argv=None):
     with open(settings.table, 'rb') as tbin:
         for row in csv.DictReader(tbin, delimiter="\t"):
             if int(row['Number of Domains']) < settings.mindomains: continue
-            best_score = float(row[row['MAP']])
-            if best_score - max([float(row[x]) for x in (set(domains.keys())-set((row['MAP'],)))]) < settings.mindiff: continue
-            if row['MAP'] == 'Archaea' and not settings.trust_archaea: continue
-            if row['MAP'] == 'Viruses' and not settings.trust_viruses: continue
-            accdom[row['Record']] = domains[row['MAP']]
+            if (float(row['Bacteria'])-float(row['Eukaryota'])>settings.mindiff):
+                accdom[row['Record']] = domains['Bacteria']
+            elif (float(row['Eukaryota'])-float(row['Bacteria'])>settings.mindiff):
+                accdom[row['Record']] = domains['Eukaryota']
     # Read the fasta and print the records passed filter to STDOUT
     for record in SeqIO.parse(settings.fasta, 'fasta'):
         if record.id.startswith("kraken"):
@@ -183,11 +162,10 @@ def main(argv=None):
             try:
                 (txid,) = curr.execute("select taxid from acc2taxid where acc=?", (nover,)).next()
             except:
-                taid = None
-#            txid = curr.fetchone()
-#            if txid: txid = txid[0]
+                txid = None
         if txid and acc in accdom:
-            if not match_txid(accdom[acc], txid, curr, settings.filter_virus, not settings.filter_enviro, settings.filter_archaea): continue
+            if not match_txid(accdom[acc], txid, curr, (domains['Eukaryota'], domains['Bacteria'])): 
+                continue
         if not txid:
             logging.warn("Can't find taxonomy ID for sequence: {}".format(record.id))
         sys.stdout.write(record.format('fasta'))
